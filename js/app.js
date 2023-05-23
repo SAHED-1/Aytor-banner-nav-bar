fe53f436eb33c7a613885c6c4b79d7e5d318ab87
IBRAHIM ULLAH SAHED <123239894+SAHED-1@users.noreply.github.com>
2023-05-23 12:31:27 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// BANNER 1 slider=====================================================================
+$(document).ready(function(){
+    $('#banner').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+    });
+  });
+// BANNER 1 slider end=====================================================================
